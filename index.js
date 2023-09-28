d133cfd87372c745b67a9e904c9e6ba82ec8ce0e
Egrinaldo Junior <egrinaldo19@gmail.com>
2023-09-27 22:19:18 -0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert('Olá Mundo');
+// document.getElementById('texto').innerHTML= 'Meu texto JS';
+// alert(10+5);
+// console.log('Mais silencioso')
+
+// var nome = 'naldo'
+
+// console.log(nome)
+
+const prompt= require('readline-sync');
+
+const idade = prompt.question('Qual a sua idade');
+
+console.log(idade);
